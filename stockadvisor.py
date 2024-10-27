f346c81a3c740794ecf31cde38672b18cf0044de
Zachary5349 <zacharyjasonwright@gmail.com>
2024-10-27 10:10:19 -0500

    Update stockadvisor.py
    added graphical features

--- stockadvisor.py
+++ stockadvisor.py
@@ -1,61 +1,111 @@
-
-import streamlit as st
-
-from langchain.llms import OpenAI
 from langchain.agents import AgentType, initialize_agent, load_tools
 from langchain.callbacks import StreamlitCallbackHandler
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.prompts import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chains.llm import LLMChain
+from langchain.schema import ChatMessage
+from langchain.llms import OpenAI
+from datetime import timedelta
 from datetime import datetime
-import asyncio
-import yfinance as yf
-import pandas as pd
-import plotly.graph_objects as go
+import streamlit as st
+from PIL import Image
+import datetime as dt
 import requests
+import asyncio
+import base64
+import time
 import json
-# This library will be used to fetch the API.
+import numpy
 import urllib.request
 
-
 st.set_page_config(page_title = "Dater", page_icon = "🌌️", layout = "wide", initial_sidebar_state = "expanded")
-st.title("Dater 👁️‍🗨️️")
+container = st.container()
 
-OPENAI_API_KEY = "API KEY HERE"
+img = img.resize((300, 75))
+container.image(img)
 
-class Dater:
-    
+openai_api_key = "Enter OPENAI Api Key"
+
+class Background:
+    def __init__(self, img):
+        self.img = img
+    def set_back_side(self):
+        side_bg_ext = 'png'
+        side_bg = self.img
+
+        st.markdown(
+        f"""
+        <style>
+            [data-testid="stSidebar"] > div:first-child {{
+                background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()});
+            }}
+        </style>
+        """,
+        unsafe_allow_html = True,
+        )
+
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container, initial_text = ""):
+        self.container = container
+        self.text = initial_text
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        self.text += token
+        self.container.markdown(self.text)
+
+class Fundamental:
     def __init__(self):
-        self.llm = OpenAI(model_name = "gpt-4o", temperature = 0, streaming = True, api_key = OPENAI_API_KEY)
+        
+        self.query = "Please, give me an exahustive fundamental analysis about the companies that you find in the documented knowledge. I want to know the pros and cons of a large-term investment. Please, base your answer on what you know about the company, but also on wht you find useful about the documented knowledge. I want you to also give me your opinion in, if it is worthy to invest on that company given the fundamental analysis you make. If you conclude that is actually wise to invest on a given company, or in multiple companies (focus only on the ones in the documented knowledge) then come up also with some strategies that I could follow to make the best out of my investments."
+        self.llm = OpenAI(model_name = "gpt-4o", temperature = 0, streaming = True, api_key = openai_api_key)
         self.tools = load_tools(["ddg-search"])
-        self.agent = initialize_agent(self.tools, self.llm, agent = AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose = False, handle_parsing_errors=True)
+        self.agent = initialize_agent(self.tools, self.llm, agent = AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose = False)
+        self.prompt_template = " stock symbol(s). Return only the symbols separated by spaces. Don't add any type of punctuantion. If you already know the answer, there is no need to search for it on duckduck go"
+        self.DEFAULT_TEMPLATE = """The following is a friendly conversation between a human and an AI. The
+        AI is an AI-powered fundamental analist. It uses Documented Information to give fundamental insights on an assest detemrined by the user. It is specific, and gives full relevant
+        perspective to let the user know, if it is worthy to make investment on a certain asset, and can also build intelligent strategies with the given information, as well as from intel that it
+        already knows, or will generate. Take into account, that the documented knowledge comes in the next scrutcture. **Title:** (Message of the Titel), **Description:** (Message of the
+        Descricption)\n\n, and so on. All articles from the documented knowledge have a title and a description (both of which are separated by comas), and all articles are separated with the \n\n
+        command between one another.
+
+        Documented Information:
+        {docu_knowledge},
+
+        (You do not need to use these pieces of information if not relevant)
+
+        Current conversation:
+        Human: {input}
+        AI-bot:"""
 
     async def get_stock_symbol(self, company_name):
         st_callback = StreamlitCallbackHandler(st.container())
-        search_results = self.agent.run(company_name + " stock symbol(s). Return only the symbols separated by spaces. Don't add any type of punctuantion", callbacks = [st_callback])
+        search_results = self.agent.run(company_name + self.prompt_template, callbacks = [st_callback])
         symbols = search_results.split(" ")
         return symbols
 
-    async def get_stock_history(self, symbol, date):
-        ticker = yf.Ticker(symbol)
-        data = ticker.history(start = "2015-01-01", end = date)
-        return data
-
-    async def get_gnews_api(self):
-        url = "https://gnews.io/api/v4/top-headlines?lang=en&token=<45107f25676fba4d2910653f3ad44cf9>"
+    def get_gnews_api(self):
+        url = "https://gnews.io/api/v4/top-headlines?lang=en&token=<Enter GNews Api Key>"
         response = requests.get(url)
         news = response.json()
-        return news 
+        return news
 
-    async def get_gnews_api_spec(self, search_term):
-          
-        apikey = "45107f25676fba4d2910653f3ad44cf9"
-        search_term = "nvidia"
-        url = f"https://gnews.io/api/v4/search?q={search_term}&lang=en&country=us&max=10&apikey={apikey}"
-        with urllib.request.urlopen(url) as response:
-            data = json.loads(response.read().decode("utf-8"))
-            articles = data["articles"]
-            return articles
+    def get_gnews_api_spec(self, search_term):
+        url = f"https://gnews.io/api/v4/search?q={search_term}&token=<Enter GNews Api Key>"
+        response = requests.get(url)
+        news = response.json()
+        return news
 
-        
-        
+    def get_response(self, user_message, docu_knowledge, temperature = 0):
+
+        PROMPT = PromptTemplate(input_variables = ['input','docu_knowledge'], template = self.DEFAULT_TEMPLATE)
+
+        stream_handler = StreamHandler(st.empty())
+        chat_gpt = ChatOpenAI(streaming = True, callbacks = [stream_handler], temperature = temperature, model_name = "gpt-4o")
+        conversation_with_summary = LLMChain(llm = chat_gpt, prompt = PROMPT, verbose = True)
+        output = conversation_with_summary.predict(input = user_message, docu_knowledge = docu_knowledge)
+
+        return output
 
     async def run(self):
         date_now = datetime.now()
@@ -66,68 +116,99 @@ class Dater:
 
         date_d = "{}-{}-{}".format(date_year, date_month, date_day)
 
-        st.title(":blue[Welcome!:]")
-        st.header("_Trading Bot App_")
-        st.subheader(f" :green[_{date_d}_]")
-        st.subheader(date_day_, divider = 'rainbow')
+        st.title(":orange[Welcome!]")
+        st.subheader(f" _{date_d}_")
+        st.subheader(f" :orange[_{date_day_}_]", divider = 'rainbow')
 
-        company_name = st.text_input("Enter a company name:")
+        if "messages" not in st.session_state:
+            st.session_state["messages"] = [ChatMessage(role = "assistant", content = "")]
 
-        if company_name:
+        with st.form(key = 'company_search_form'):
+            company_name = st.text_input("Enter a company name:")
+            submit_button = st.form_submit_button("Search", type = "primary")
 
+        if submit_button and company_name:
+            articles_string = ""
             symbols = await self.get_stock_symbol(company_name)
+            cont_api = 0
 
             for symbol in symbols:
+                apikey = "Enter GNews Api Key"
+                search_term = symbol
+                url = f"https://gnews.io/api/v4/search?q={search_term}&lang=en&country=us&max=10&apikey={apikey}"
 
-                
-                left_column, right_column = st.columns(2)
+                with urllib.request.urlopen(url) as response:
+                    data = json.loads(response.read().decode("utf-8"))
+                    articles = data["articles"]
 
-                with left_column:
-                    apikey = "45107f25676fba4d2910653f3ad44cf9"
-                    search_term = "nvidia"
-                    url = f"https://gnews.io/api/v4/search?q={search_term}&lang=en&country=us&max=10&apikey={apikey}"
+                       
+                    with st.spinner("Searching..."):
+                    
+                        with st.sidebar:
+                            with st.expander(symbol):
 
-                    with urllib.request.urlopen(url) as response:
-                        data = json.loads(response.read().decode("utf-8"))
-                        articles = data["articles"]
+                                st.subheader("News from GNews API", divider = 'rainbow')
+                                for i in range(len(articles)):
+                                #print(f"Title: {articles[i]['title']}")
 
-                        for i in range(len(articles)):
-                            #print(f"Title: {articles[i]['title']}")
-                            #print(f"Description: {articles[i]['description']}")
-                            st.write(f"**Title:** {articles[i]['title']}")
-                            st.write(f"**Description:** {articles[i]['description']}")
-                            st.write(f"**URL:** {articles[i]['url']}")
-                            st.markdown("""---""")
-       
-                            break
-                with right_column:
-                    plot_placeholder = st.empty()
+                                #print(f"Description: {articles[i]['description']}")
+                                    st.write(f"**Title:** {articles[i]['title']}")
+                                    st.write(f"**Description:** {articles[i]['description']}")
+                                    st.write(f"**URL:** {articles[i]['url']}")
+                                    st.markdown("""---""")
+            try:
+                with st.sidebar:
+                    with st.spinner("Searching..."):
+                        
+                        with st.expander("General News"):
+                            apikey2 = "Enter GNews Api Key"
+                           
+                            url = f"https://gnews.io/api/v4/top-headlines?lang=en&token=<apikey2>"
+                            with urllib.request.urlopen(url) as response:
+                                data = json.loads(response.read().decode("utf-8"))
+                                articles = data["articles"]
+                                
 
-                st.markdown("""---""")
 
-                df = await self.get_stock_history(symbol, date_d)
+                                with st.spinner("Searching..."):
+                    
+                                    with st.sidebar:
+                                        with st.expander("General News"):
 
-                data_date = df.index.to_numpy()
-                data_open_price = df['Open'].to_numpy()
-                data_high_price = df['High'].to_numpy()
-                data_low_price = df['Low'].to_numpy()
-                data_close_price = df['Close'].to_numpy()
 
-                feg = go.Figure(data = [go.Candlestick(x = data_date, open = data_open_price, high = data_high_price, low = data_low_price, close = data_close_price)])
-                feg.update_layout(title_text = "Full Data")
-                feg.update_layout(xaxis_rangeslider_visible = False)
+                                            st.subheader("General News from GNews API", divider = 'rainbow')
+                                            for i in range(len(articles)):
+                                                    
+                                                #print(f"Title: {articles[i]['title']}")
+                                                #print(f"Description: {articles[i]['description']}")
+                                                st.write(f"**Title:** {articles[i]['title']}")
+                                                st.write(f"**Description:** {articles[i]['description']}")
+                                                
+                                                st.write(f"**URL:** {articles[i]['url']}")
+                                                st.markdown("""---""")
+                                                st.markdown("""---""")
 
-                plot_placeholder.plotly_chart(feg, use_container_width = True)
-            '''
-            gnews_api = await self.get_gnews_api()
-            st.write("## News from GNews API")
-            for article in gnews_api['articles']:
-                st.write(f"**Title:** {article['title']}")
-                st.write(f"**Description:** {article['description']}")
-                st.write(f"**URL:** {article['url']}")
-                st.write("---")
-                '''
+                                                df = await self.get_stock_history(symbol, date_d)
+
+                                                data_date = df.index.to_numpy()
+                                                data_open_price = df['Open'].to_numpy()
+                                                data_high_price = df['High'].to_numpy()
+                                                data_low_price = df['Low'].to_numpy()
+                                                data_close_price = df['Close'].to_numpy()
+
+                                                feg = go.Figure(data = [go.Candlestick(x = data_date, open = data_open_price, high = data_high_price, low = data_low_price, close = data_close_price)])
+                                                feg.update_layout(title_text = "Full Data")
+                                                feg.update_layout(xaxis_rangeslider_visible = False)
+
+                                                plot_placeholder.plotly_chart(feg, use_container_width = True)
+                                                
+            
+
+                with st.chat_message("assistant"):
+                    user_input = self.query
+                    output = self.get_response(user_input, articles_string, temperature = 0)
+                    st.session_state.messages.append(ChatMessage(role = "assistant", content = output))
 
 if __name__ == "__main__":
-    dater = Dater()
-    asyncio.run(dater.run())
+    fundamental = Fundamental()
+    asyncio.run(fundamental.run())
